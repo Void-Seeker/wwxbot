bf1f6cc57be022adb4a72fa19292d760a9ea0832
Void-Seeker <gausshunter@gmail.com>
2018-08-30 15:00:56 +0300

    Added aprs.fi api call for sender, added beaconing in another thread

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import aprslib
 import logging
+import time
+import threading
+import requests
+import json
 logging.basicConfig(level=logging.DEBUG)
 
 callsign = "UR5RFF-15"
 password = '21924'
 
-def antitrim(line, char, reqLength):
-    if (len(line) < reqLength):
-        return line + (reqLength - len(line)) * char
+
+def antitrim(line, char, reqlength):
+    if len(line) < reqlength:
+        return line + (reqlength - len(line)) * char
     else:
         return line
+
+
+def beacon():
+    global AIS
+    while True:
+        AIS.sendall(callsign + '>APRS,TCPIP*:=5131.45NA03045.83EWweather bot')
+        time.sleep(600)
+
+
+def respond(msg):
+    print(msg)
+    r = requests.get('https://api.aprs.fi/api/get?name=' + msg['from']
+                     + '&what=loc&apikey=108829.nt375IMvw8nlF&format=json').json()
+    if r['found'] != 1:
+        print('Warning: Found ' + r['found'] + ' entries')
+    sender = r['entries'][0]
+    print(sender)
+
 
 def callback(packet):
     try:
@@ -20,12 +43,16 @@ def callback(packet):
             if len(spec) == 2:
                 line = callsign+'>APRS,TCPIP*::'+antitrim(obj['from'], ' ', 9)+':ack'+spec[-1]
                 AIS.sendall(line)
-    except  (aprslib.ParseError) as exp:
+            obj['message_text'] = spec[0]
+            respond(obj)
+    except aprslib.ParseError as exp:
         print(packet)
 
 
 AIS = aprslib.IS(callsign, passwd=password, port=14580)
 AIS.connect()
-AIS.sendall(callsign+'>APRS,TCPIP*:=5131.45NA03045.83EWweather bot')
-
-AIS.consumer(callback, raw=True)
+# AIS.sendall(callsign + '>APRS,TCPIP*:=5131.45NA03045.83EWweather bot')
+t = threading.Thread(target=beacon)
+t.start()
+AIS.consumer(callback, raw=True, blocking=True)
+AIS.close()
